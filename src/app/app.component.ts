927f3d245b051ede05b0f6c6fd8d77eef23c139f
Rodrigo A. Silva <rgbrasil2@gmail.com>
2024-01-13 13:44:04 -0300

    Aula 03 - Crud Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'crud-angular';
