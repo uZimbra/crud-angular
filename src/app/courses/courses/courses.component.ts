f573f3d769cd46585e944bc67643ae5dc7af15dd
Rodrigo A. Silva <rgbrasil2@gmail.com>
2024-01-13 18:51:15 -0300

    Aula 06 - Angular Crud

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../models/courses';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.scss'
+  styleUrl: './courses.component.scss',
 })
 export class CoursesComponent {
-
+  courses: Course[] = [
+    { _id: '1', name: 'Curso de NestJS', category: 'backend' },
+    { _id: '2', name: 'Curso de Springboot', category: 'backend' },
+    { _id: '3', name: 'Curso de React', category: 'frontend' },
+    { _id: '4', name: 'Curso de Angular', category: 'frontend' },
+  ];
+  displayedColumns = ['name', 'category'];
 }
